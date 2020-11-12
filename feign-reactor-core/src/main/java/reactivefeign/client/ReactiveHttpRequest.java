a94422a0588060c213ad34ffb7c2582429ccbf64
Ильиных Илья Сергеевич <IIlyinykh@alfabank.ru>
2020-11-12 19:13:21 +0300

    Reset Request class

--- feign-reactor-core/src/main/java/reactivefeign/client/ReactiveHttpRequest.java
+++ feign-reactor-core/src/main/java/reactivefeign/client/ReactiveHttpRequest.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2018 The Feign Authors
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -26,7 +26,7 @@ import static reactivefeign.utils.FeignUtils.methodTag;
 
 /**
  * An immutable reactive request to an http server.
- *
+ * 
  * @author Sergii Karpenko
  */
 public final class ReactiveHttpRequest {
@@ -54,14 +54,15 @@ public final class ReactiveHttpRequest {
     this(request.methodMetadata, request.target, uri, request.headers, request.body);
   }
 
-  public ReactiveHttpRequest(ReactiveHttpRequest request, Publisher<Object> body) {
-    this(request.methodMetadata, request.target, request.uri, request.headers, body);
+  public ReactiveHttpRequest(ReactiveHttpRequest request, Publisher<Object> body){
+     this(request.methodMetadata, request.target, request.uri, request.headers, body);
   }
 
   /* Method to invoke on the server. */
   public String method() {
     return methodMetadata.template().method();
   }
+
 
 
   /* Fully resolved URL including query. */
@@ -81,7 +82,7 @@ public final class ReactiveHttpRequest {
     return body;
   }
 
-  public String methodKey() {
+  public String methodKey(){
     return methodTag(methodMetadata);
   }
 
