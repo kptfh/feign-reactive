71e68f8cb855e03ec6381aee59a0f14e261849f0
Sergii Karpenko <skarpenko@playtika.com>
2020-12-01 19:34:52 +0200

    added test that returns raw(byte[]) ResponseEntity

--- feign-reactor-webclient/src/test/java/reactivefeign/webclient/ResponseEntityTest.java
+++ feign-reactor-webclient/src/test/java/reactivefeign/webclient/ResponseEntityTest.java
@@ -1,25 +1,21 @@
 package reactivefeign.webclient;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import feign.RequestLine;
 import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-import reactivefeign.testcase.IcecreamServiceApi;
-import reactivefeign.testcase.domain.Flavor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.Arrays;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static java.util.Arrays.asList;
-import static reactivefeign.TestUtils.MAPPER;
 import static reactivefeign.TestUtils.toLowerCaseKeys;
-import static reactivefeign.webclient.WebReactiveFeign.builder;
 
 public class ResponseEntityTest {
 
@@ -49,8 +45,34 @@ public class ResponseEntityTest {
                 .verifyComplete();
     }
 
+    @Test
+    public void shouldPassResponseAsRawResponseEntity() {
+
+        wireMockRule.stubFor(get(urlEqualTo("/" + TEST_URL))
+                .willReturn(aResponse().withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("[1, 2]")));
+
+        TestCaller client = WebReactiveFeign.<TestCaller>builder()
+                .target(TestCaller.class, "http://localhost:" + wireMockRule.port());
+
+        Mono<ResponseEntity<Mono<byte[]>>> resultRaw = client.callRaw();
+
+        StepVerifier.create(resultRaw)
+                .expectNextMatches(response -> toLowerCaseKeys(response.getHeaders())
+                        .containsKey("content-type"))
+                .verifyComplete();
+
+        StepVerifier.create(resultRaw.flatMapMany(HttpEntity::getBody))
+                .expectNextMatches(bytes -> Arrays.equals("[1, 2]".getBytes(), bytes))
+                .verifyComplete();
+    }
+
     public interface TestCaller {
         @RequestLine("GET " + TEST_URL)
         Mono<ResponseEntity<Flux<Integer>>> call();
+
+        @RequestLine("GET " + TEST_URL)
+        Mono<ResponseEntity<Mono<byte[]>>> callRaw();
     }
 }
